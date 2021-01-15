b85a92ce93ff5ea40d697e76dbed0f1873a0bf53
Arunoday kumar <201951031@iiitvadodara.ac.in>
2021-01-15 16:21:57 +0530

    first commit'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'Quiz_brain.dart';
+
+QuizBrain quizBrain= QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> score_timeline = [];
+  void checkAnswers(bool userPickedAnwers){
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState(() {
+    if(userPickedAnwers == true){
+     score_timeline.add(Icon(
+           Icons.check,
+           color: Colors.green,
+         ));
+
+    }
+    if(userPickedAnwers == false){
+      score_timeline.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+    }
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +85,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+
+                checkAnswers(true);
                 //The user picked true.
+
               },
             ),
           ),
@@ -79,19 +106,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswers(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: score_timeline
+
+
+
+        )
+
+
       ],
+
     );
+
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ lib/questions.dart
@@ -0,0 +1,10 @@
+class Questions{
+  String questionText;
+  bool answers;
+
+  Questions(String q, bool a){
+    questionText = q;
+    answers = a;
+  }
+}
+//Questions newQuestion = Questions(q: , a:);
